de222c42c9318d6624a5a0a90f40020b51ab1f19
jiel <nikhilpagadala2006@gmail.com>
2026-01-12 11:08:40 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://candid-daffodil-95ad2f.netlify.app/',
   credentials: true,
 }));
 app.use(express.json());
